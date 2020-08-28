33c42752ce3490c615f45fe5fb3f3be583fa50ed
Everett Price <evprice@microsoft.com>
2020-08-28 15:21:39 -0700

    added a linter wooo

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var request = require('request')
-var request = request.defaults({jar:true})
-var cookies = request.jar()
-var credentials = require('./credentials.json')
-var classes = require('./data/classes.json')
+let request = require('request');
+request = request.defaults({jar: true});
+let cookies = request.jar();
+let credentials = require('./credentials.json');
+let classes = require('./data/classes.json');
+let schedule = require('./schedule.json');
+// https://studiobookingsonline.com/proclub-belfitness/clientclasscalendar/index/id/37647/id/81
 
-//https://studiobookingsonline.com/proclub-belfitness/clientclasscalendar/index/id/37647/id/81
 
+// 6 PM unsupervised workout is 40 for weekdays and 98 for weekends ?
 
-//6 PM unsupervised workout is 40 for weekdays and 98 for weekends ?
+// 20078_40 8/25
+// 20079_40 8/26
+// 20080_40 8/27
+// 20081_40 8/28
 
-//20078_40 8/25
-//20079_40 8/26
-//20080_40 8/27
-//20081_40 8/28 
-
-//weekend (sat/sun slots) look like this
-//43883_98 8/29
-//43884_98 8/30
-//43885_98 9/05
+// weekend (sat/sun slots) look like this
+// 43883_98 8/29
+// 43884_98 8/30
+// 43885_98 9/05
 
 // 3 chars ? -> incremented value -> recurrclassid
 // classid
 
-///proclub-belfitness/clientclasscalendar/index/id/20078/id/40
+// proclub-belfitness/clientclasscalendar/index/id/20078/id/40
 
-/*const API = {
+/* const API = {
     SIGNIN : {
         URL: 'https://studiobookingsonline.com/proclub-belfitness/login/signin',
         FORM: {
@@ -39,7 +39,7 @@ var classes = require('./data/classes.json')
     CHECK_AVAILABILITY: {
         URL: 'https://studiobookingsonline.com/proclub-belfitness/clientclasscalendar/checkavailable',
         FORM: {
-            classids: classid 
+            classids: classid
         }
     },
     BOOK_MULTIPLE_CLASS: {
@@ -52,7 +52,23 @@ var classes = require('./data/classes.json')
     }
 }*/
 
-var today = new Date()
+
+// all this date logic will probably break if your computer doing weird things
+// maybe one day it will get fixed to unix time
+const TODAY = new Date();
+const CURRENT_DAY_OF_WEEK = TODAY.getDay();
+const CURRENT_HOUR = TODAY.getHours();
+
+// use military hours in the schedule.json file (18 -> 6pm)
+const daysToCheck = [];
+for (let i = 0; i < 3; i++) {
+    daysToCheck.push((currDayOfTheWeek + i) % 7);
+}
+
+for (const day in daysToCheck){
+
+}
+
 console.log(today)
 
 /*request.post({url: API.SIGNIN.URL, jar: cookies, form:API.SIGNIN.FORM}, 
@@ -71,6 +87,5 @@ console.log(today)
             }
         )    
     }
-)*/
-    
+)*/    
 
